005247efe2e0f1ba6677635f1fdbdefb2b39b0a3
JonhLosten <JonhLosten@users.noreply.github.com>
2025-10-28 23:30:27 +0000

    chore: update gh-pages 5f7cb0c3dffc34b385265c98d65c7a89690645bf

--- app/squirrel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    electron_1.app.quit();
-}
--- src/lib/api.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetchSnapshot = fetchSnapshot;
-async function fetchSnapshot(days = 30) {
-    const res = await fetch('http://localhost:5174/api/snapshot?days=' + days);
-    if (!res.ok)
-        throw new Error(await res.text());
-    return res.json();
-}
